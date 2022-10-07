fb39b66b8f312322f99450cea5bb099a6954b63c
nbwzx <nbwzx@126.com>
2022-10-08 00:50:31 +0800

    Fix a bug in `setCookie`

--- assets/js/setcode.js
+++ assets/js/setcode.js
@@ -6,7 +6,7 @@ function setCookie(cname, cvalue, exdays) {
     const d = new Date();
     d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
     const expires = `expires=${d.toGMTString()}`;
-    document.cookie = `${cname}=${cvalue};${expires};`;
+    document.cookie = `${cname}=${cvalue};${expires};path=/`;
 }
 
 function getCookie(cname) {
