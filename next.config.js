fb9b83cfc519670657ccb3654b952022b364c397
nbwzx <nbwzx@126.com>
2024-03-14 13:38:26 +0800

    chore: update config files

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["localhost"],
     remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "3000",
+      },
       {
         protocol: "https",
         hostname: "cdn.sanity.io",
