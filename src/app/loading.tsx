95ef6ae5759e77cb27347659446b032230d14110
nbwzx <nbwzx@126.com>
2024-04-06 20:22:00 +0800

    feat: add loading page

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <>
+      <section className="relative z-10 pb-16 pt-36 md:pb-20 lg:pb-28 lg:pt-[180px]"></section>
+    </>
+  );
+};
+
+export default Loading;
