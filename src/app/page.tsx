fd4fbced3022f768b1995b44cff56b92da9bff6c
nbwzx <nbwzx@126.com>
2025-01-30 20:19:37 +0800

    refactor: add Google Tag Manager

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import Features from "@/components/Features";
 import Hero from "@/components/Hero";
 import Footer from "@/components/Footer";
 import { Metadata } from "next";
+import React from "react";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   other: {
@@ -14,6 +16,21 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <>
+      {process.env.NODE_ENV === "production" && (
+        <React.Fragment>
+          <Script
+            id="next"
+            async
+            src={"https://www.googletagmanager.com/gtag/js?id=G-EGQN6HF7KE"}
+          ></Script>
+          <Script id="next">
+            {`window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-EGQN6HF7KE');`}
+          </Script>
+        </React.Fragment>
+      )}
       <ScrollUp />
       <Hero />
       <Features />
