4782cae6082765cd2849992cfa70735228f67ca9
nbwzx <nbwzx@126.com>
2025-08-01 10:11:28 +0800

    refactor: improve error handling in result.py

--- scripts/result.py
+++ scripts/result.py
@@ -1,6 +1,7 @@
 import json
 import requests
 import sys
+import time
 
 from bs4 import BeautifulSoup
 from loguru import logger
@@ -29,45 +30,65 @@ def main():
 
     for name in algs_json:
         search_url = f"https://www.worldcubeassociation.org/api/v0/search?q={name}"
-        response = requests.get(search_url)
 
-        if response.status_code == 200:
-            search_results = response.json().get("result")
-            for search_result in search_results:
-                if search_result.get("name") != name:
-                    continue
-
-                result_name = {}
-                wca_id_text = search_result.get("wca_id")
-                result_name["wca_id"] = wca_id_text
-                person_url = (
-                    f"https://www.worldcubeassociation.org/persons/{wca_id_text}"
-                )
-                response = requests.get(person_url)
-
+        while True:
+            try:
+                response = requests.get(search_url, timeout=10)
                 if response.status_code == 200:
-                    soup = BeautifulSoup(response.text, "html.parser")
-                    links = soup.find_all("a")
-                    for link in links:
-                        if link.get("href") == "/results/rankings/333bf/single":
-                            result_name["3bld"] = parse_time(link.text.strip())
-                        if link.get("href") == "/results/rankings/444bf/single":
-                            result_name["4bld"] = parse_time(link.text.strip())
+                    search_results = response.json().get("result")
+                    break
                 else:
-                    logger.error(
-                        f"Request to {person_url} failed with status code: {response.status_code}"
+                    logger.warning(
+                        f"Request to search URL failed for {name} with status code: {response.status_code}"
                     )
-                if name not in results:
-                    results[name] = result_name
-                else:
-                    if "3bld" in result_name and ("3bld" not in results[name] or result_name["3bld"] < results[name]["3bld"]):
-                        results[name]["wca_id"] = result_name["wca_id"]
-                        results[name]["3bld"] = result_name["3bld"]
-                        results[name]["4bld"] = result_name["4bld"]
-        else:
-            logger.error(
-                f"Request to search URL failed for {name} with status code: {response.status_code}"
-            )
+                    time.sleep(10)
+            except Exception as e:
+                logger.warning(e.__class__.__name__ +
+                                " when opening " + search_url)
+                time.sleep(10)
+
+        for search_result in search_results:
+            if search_result.get("name") != name:
+                continue
+
+            result_name = {}
+            wca_id_text = search_result.get("wca_id")
+            result_name["wca_id"] = wca_id_text
+            person_url = f"https://www.worldcubeassociation.org/persons/{wca_id_text}"
+
+            while True:
+                try:
+                    response = requests.get(person_url, timeout=10)
+
+                    if response.status_code == 200:
+                        soup = BeautifulSoup(response.text, "html.parser")
+                        links = soup.find_all("a")
+                        for link in links:
+                            if link.get("href") == "/results/rankings/333bf/single":
+                                result_name["3bld"] = parse_time(link.text.strip())
+                            if link.get("href") == "/results/rankings/444bf/single":
+                                result_name["4bld"] = parse_time(link.text.strip())
+                        break
+                    else:
+                        logger.warning(
+                            f"Request to {person_url} failed with status code: {response.status_code}"
+                        )
+                        time.sleep(10)
+                except Exception as e:
+                    logger.warning(e.__class__.__name__ +
+                                   " when opening " + person_url)
+                    time.sleep(10)
+
+            if name not in results:
+                results[name] = result_name
+            else:
+                if "3bld" in result_name and (
+                    "3bld" not in results[name]
+                    or result_name["3bld"] < results[name]["3bld"]
+                ):
+                    results[name]["wca_id"] = result_name["wca_id"]
+                    results[name]["3bld"] = result_name["3bld"]
+                    results[name]["4bld"] = result_name["4bld"]
 
         logger.info(f"{name}: {results.get(name)}")
 
