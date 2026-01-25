5df2dd8d0280a9746826bfa9b9788635621000ae
nbwzx <nbwzx@126.com>
2026-01-25 10:12:14 +0800

    refactor: change language without reloading

--- src/components/Header/switchLocale.ts
+++ src/components/Header/switchLocale.ts
@@ -1,8 +1,10 @@
 "use client";
 
+import i18next from "i18next";
 export async function switchLocaleAction(value: string) {
   // Set cookie directly in browser
   document.cookie = `i18next=${value}; path=/; max-age=34560000`;
-  // Refresh the page
-  window.location.reload();
+
+  // Change language without reloading
+  await i18next.changeLanguage(value);
 }
