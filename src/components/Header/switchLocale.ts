dc0c3422944a90b607dd4d9286fe90fc8b13f0c4
nbwzx <nbwzx@126.com>
2026-02-18 22:54:11 +0800

    Revert "refactor: change language without reloading"
    This reverts commit 5df2dd8d0280a9746826bfa9b9788635621000ae.

--- src/components/Header/switchLocale.ts
+++ src/components/Header/switchLocale.ts
@@ -1,10 +1,8 @@
 "use client";
 
-import i18next from "i18next";
 export async function switchLocaleAction(value: string) {
   // Set cookie directly in browser
   document.cookie = `i18next=${value}; path=/; max-age=34560000`;
-
-  // Change language without reloading
-  await i18next.changeLanguage(value);
+  // Refresh the page
+  window.location.reload();
 }
