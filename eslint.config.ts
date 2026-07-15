ec6d040d51312dad587d033820196895a4e92775
nbwzx <nbwzx@126.com>
2026-07-15 14:20:00 +0800

    fix: remove unused '@ts-expect-error' directive

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,7 +1,5 @@
-// @ts-expect-error -- ts can't find types for eslint-config-next
 import nextTypescript from "eslint-config-next/typescript";
 import { defineConfig } from "eslint/config";
-// @ts-expect-error -- ts can't find types for eslint-config-next
 import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
