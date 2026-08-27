6ef5263ddcc62dbc37e5dc8c5fe89e6a8129e12b
nbwzx <nbwzx@126.com>
2026-08-27 13:41:12 +0800

    refactor: remove unused dead code

--- src/types/brand.ts
+++
@@ -1,7 +0,0 @@
-export type Brand = {
-  id: number;
-  name: string;
-  href: string;
-  image: string;
-  imageLight?: string;
-};
--- src/types/testimonial.ts
+++
@@ -1,8 +0,0 @@
-export type Testimonial = {
-  id: number;
-  name: string;
-  designation: string;
-  content: string;
-  image: string;
-  star: number;
-};
